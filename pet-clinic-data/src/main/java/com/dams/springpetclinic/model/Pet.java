75ad4e7cd744dae9d73d1b84da31790c9a694ca8
Dário Silva <dario.silva@vilt-group.com>
2021-04-01 15:51:03 +0100

    Adding base entity.

--- pet-clinic-data/src/main/java/com/dams/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/dams/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.dams.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
