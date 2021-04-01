c65321dd453497a4d1622251c9863f38a8965bd9
Dário Silva <dario.silva@vilt-group.com>
2021-04-01 16:21:19 +0100

    Impl Map based Services.

---
+++ pet-clinic-data/src/main/java/com/dams/springpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.dams.springpetclinic.services.map;
+
+import com.dams.springpetclinic.model.Vet;
+import com.dams.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
