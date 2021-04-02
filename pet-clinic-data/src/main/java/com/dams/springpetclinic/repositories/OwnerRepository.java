8e55e0a43ad2eb24c97814b35767a2ddca0d621d
Dário Silva <dario.silva@vilt-group.com>
2021-04-02 14:55:44 +0100

    Added Spring data jpa service for owners.

--- pet-clinic-data/src/main/java/com/dams/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/dams/springpetclinic/repositories/OwnerRepository.java
@@ -4,5 +4,5 @@ import com.dams.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+    Owner findByLastName(String name);
 }
