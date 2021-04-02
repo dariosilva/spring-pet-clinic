9eeaad50e09a2aabcdf7405c40c58e9912e07c55
Dário Silva <dario.silva@vilt-group.com>
2021-04-02 12:12:17 +0100

    Added Specialities Map Impl.

---
+++ pet-clinic-data/src/main/java/com/dams/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.dams.springpetclinic.services;
+
+import com.dams.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/dams/springpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package com.dams.springpetclinic.services.map;
+
+import com.dams.springpetclinic.model.Speciality;
+import com.dams.springpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
