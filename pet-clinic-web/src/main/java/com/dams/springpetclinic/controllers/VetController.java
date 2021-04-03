38a90d69dfe9c3c695e15065517719a5b6f33ccf
Dário Silva <dario.silva@vilt-group.com>
2021-04-03 15:00:55 +0100

    Added json endpoint to get all vets.

--- pet-clinic-web/src/main/java/com/dams/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/dams/springpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.dams.springpetclinic.controllers;
 
+import com.dams.springpetclinic.model.Vet;
 import com.dams.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +22,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
