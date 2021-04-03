ae4564dab4e7c7edfcfb76b5e68c341a695e9ecd
Dário Silva <dario.silva@vilt-group.com>
2021-04-03 11:12:26 +0100

    Impl find Owner.

--- pet-clinic-data/src/main/java/com/dams/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/dams/springpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package com.dams.springpetclinic.repositories;
 import com.dams.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String name);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/dams/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/dams/springpetclinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package com.dams.springpetclinic.services;
 
 import com.dams.springpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
